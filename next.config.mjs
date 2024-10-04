b4a11391f98f7dd651b0b976c4cd7db2c1bb7fcf
Giordan Masen <89724620+brebbly@users.noreply.github.com>
2024-10-04 22:03:53 +0930

    Upload and Library

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "wgmedjbqnsozhmmaoxfl.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
